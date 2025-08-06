6748d4fa3138f6d7d79a269cb1beddfed7fb70d9
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-06 20:14:50 +0600

    Update for_loop.cpp

--- Loops/for_loop.cpp
+++ Loops/for_loop.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // For loop to print numbers from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        cout << "Number: " << i << std::endl;
+    }
 
+    return 0;
+}
